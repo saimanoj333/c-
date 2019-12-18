5d1005f64b3887663c485d1755e005fe6a19e9cf
saimanoj333 <58930046+saimanoj333@users.noreply.github.com>
2019-12-18 12:28:55 +0530

    Add files via upload

---
+++ angles of triangle.cpp
@@ -0,0 +1,9 @@
+int main()
+{
+  float firstAngle,secondAngle,thirdAngle;
+  printf("Enter first and second angle of the triangle : ");
+  scanf("%f %f",&firstAngle,&secondAngle);
+  thirdAngle = SUM - (firstAngle + secondAngle);
+  printf("You have entered %f and %f\n",firstAngle,secondAngle);
+  printf("Third Angle is : %f \n",thirdAngle);
+}
